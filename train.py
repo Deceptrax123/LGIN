Fix backpropagation errors

train.py
@@ -21,8 +21,7 @@ def train_epoch():
         input = (x, adj)
 
         logits, probs = model(input, batch=data.batch)
-
-        loss = loss_function(logits, data.y)
+        loss = loss_function(logits, data.y.float())
 
         loss.backward()
 
@@ -56,7 +55,7 @@ def val_epoch():
         input = (x, adj)
 
         logits, probs = model(input, batch=data.batch)
-        loss = loss_function(logits, data.y.int())
+        loss = loss_function(logits, data.y.float())
 
         if dataset.num_classes == 2:
             acc = classification_binary_metrics(probs, data.y.int())
@@ -74,12 +73,12 @@ def val_epoch():
 def test():
     test_acc = 0
     for step, data in enumerate(test_loader):
-        x, adj = data.x, to_dense_adj(data.edge_index, batch=data.batch)
+        x, adj = data.x, data.edge_index
         input = (x, adj)
 
         logits, probs = model(input, batch=data.batch)
         if dataset.num_classes == 2:
-            acc = classification_binary_metrics(probs, data.y.int())
+            acc = classification_binary_metrics(probs, data.y.float())
         else:
             acc = classification_multiclass_metrics(
                 probs, data.y.int(), dataset.num_classes)
@@ -124,7 +123,7 @@ def training_loop():
 
 if __name__ == '__main__':
     load_dotenv('.env')
-    torch.autograd.set_detect_anomaly(True)
+    # torch.autograd.set_detect_anomaly(True)
 
     inp_name = input('Enter the dataset to be downloaded: ')
     imdb_b = os.getenv('imdb_b')
@@ -152,7 +151,7 @@ if __name__ == '__main__':
     test_ratio = 0.15
 
     params = {
-        'batch_size': 8,
+        'batch_size': 32,
         'shuffle': True,
         'num_workers': 0
     }

utils/math_utils.py
@@ -13,8 +13,8 @@ def tanh(x, clamp=15):
     return x.clamp(-clamp, clamp).tanh()
 
 
-def arcosh(x):
-    return Arcosh.apply(x)
+def arcosh(x,):
+    return x.clamp(min=1.0 + 1e-10).acosh()
 
 
 def arsinh(x):
@@ -52,15 +52,15 @@ class Arsinh(torch.autograd.Function):
         return grad_output / (1 + input ** 2) ** 0.5
 
 
-class Arcosh(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x):
-        x = x.clamp(min=1.0 + 1e-10)
-        ctx.save_for_backward(x)
-        z = x.double()
-        return (z + torch.sqrt_(z.pow(2) - 1)).clamp_min_(1e-15).log_().to(x.dtype)
+# class Arcosh(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx, x):
+#         x = x.clamp(min=1.0 + 1e-10)
+#         ctx.save_for_backward(x)
+#         z = x.double()
+#         return (z + torch.sqrt_(z.pow(2) - 1)).clamp_min_(1e-15).log_().to(x.dtype)
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, = ctx.saved_tensors
-        return grad_output / (input ** 2 - 1) ** 0.5
+#     @staticmethod
+#     def backward(ctx, grad_output):
+#         input, = ctx.saved_tensors
+#         return grad_output / (input ** 2 - 1) ** 0.5